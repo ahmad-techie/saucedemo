1e785ea90adf9115af6055ee75c9a91c3b9a50b7
Masoud Habibzad <masoud.habibzad@gmail.com>
2024-11-17 14:29:27 -0500

    Add extent reports

---
+++ src/main/java/com/saucedemo/config/Browsers.java
@@ -0,0 +1,8 @@
+package com.saucedemo.config;
+
+public class Browsers {
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String EDGE = "edge";
+    public static final String SAFARI = "safari";
+}
---
+++ src/test/java/com/saucedemo/util/TestListener.java
@@ -0,0 +1,4 @@
+package com.saucedemo.util;
+
+public class TestListener {
+}
