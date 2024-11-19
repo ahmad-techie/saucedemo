b97ac5cf1d8bc739bc3e754e3231048ba9010156
Masoud Habibzad <masoud.habibzad@gmail.com>
2024-11-19 17:09:08 -0500

    update base test class

--- src/main/java/com/saucedemo/ui/base/BasePage.java
+++ src/main/java/com/saucedemo/ui/base/BasePage.java
@@ -1,20 +1,31 @@
 package com.saucedemo.ui.base;
 
+import com.google.common.base.Function;
 import com.saucedemo.utils.ConfigReader;
 import com.saucedemo.utils.Constants;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
 
+    protected Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     protected WebDriver driver;
-    private WebDriverWait wait;
+    private final WebDriverWait wait;
+    private final boolean isDemoMode = Boolean.parseBoolean(ConfigReader.get("demoMode"));
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -45,8 +56,249 @@ public class BasePage {
                 .until(ExpectedConditions.visibilityOf(element)).getText();
     }
 
-    protected void waitForVisibilityOfElement(WebElement element, int timeout) {
-        wait.until(ExpectedConditions.visibilityOf(element));
+    public void handleCheckBox(By locator, boolean isCheck) {
+        try {
+            WebElement checkBoxElem = find(locator);
+            highlightElement(checkBoxElem);
+
+            boolean checkBoxState = checkBoxElem.isSelected();
+            // If the desired state is to check the checkbox
+            if (isCheck && !checkBoxState) {
+                checkBoxElem.click();
+            }
+            // If the desired state is to uncheck the checkbox
+            else if (!isCheck && checkBoxState) {
+                checkBoxElem.click();
+            }
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
     }
 
+    public void selectDropDown(By locator, int index) {
+        try {
+            WebElement element = driver.findElement(locator);
+            highlightElement(element);
+            Select dropDown = new Select(element);
+            dropDown.selectByIndex(index);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void selectDropDown(By locator, String visibleText) {
+        try {
+            WebElement element = driver.findElement(locator);
+            highlightElement(element);
+            Select dropDown = new Select(element);
+            dropDown.selectByVisibleText(visibleText);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void selectDropDownByAttributeValue(By locator, String attributeValue) {
+        try {
+            WebElement element = driver.findElement(locator);
+            highlightElement(element);
+            Select dropDown = new Select(element);
+            dropDown.selectByValue(attributeValue);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+
+    public void clickHiddenElement(By locator) {
+        try {
+            WebElement elem = driver.findElement(locator);
+            highlightElement(elem);
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", elem);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+
+    public void moveMouseToElement(WebElement targetElem) {
+        try {
+            highlightElement(targetElem);
+            Actions action = new Actions(driver);
+            action.moveToElement(targetElem).build().perform();
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void moveMouseToElement(WebElement firstElem, WebElement secElem) {
+        try {
+            highlightElement(firstElem);
+            Actions action = new Actions(driver);
+            action.moveToElement(firstElem).build().perform();
+            highlightElement(secElem);
+            action.moveToElement(secElem).build().perform();
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void highlightElement(WebElement element) {
+        try {
+            if (isDemoMode == true) {
+                for (int i = 0; i < 4; i++) {
+                    WrapsDriver wrappedElement = (WrapsDriver) element;
+                    JavascriptExecutor js = (JavascriptExecutor) wrappedElement.getWrappedDriver();
+                    js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element,
+                            "color: red; border: 2px solid yellow");
+                    js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "");
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void highlightElement(By locator) {
+        WebElement element = find(locator);
+        highlightElement(element);
+    }
+
+    public void scrollToElement(WebElement element) {
+        try {
+            highlightElement(element);
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView();", element);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void scrollByVertically(String verticalPixel) {
+        try {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("scroll(0," + verticalPixel + ")");
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void scrollByHorizontally(String horizontalPixel) {
+        try {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("scroll(" + horizontalPixel + ",0)");
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void sendKeyBoard(CharSequence... keysToSend) {
+        try {
+            WebElement webpage = driver.findElement(By.tagName("body"));
+            highlightElement(webpage);
+            webpage.sendKeys(keysToSend);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public String getCurrentTime() {
+        int hour = LocalTime.now().getHour();
+        int minute = LocalTime.now().getMinute();
+        int sec = LocalTime.now().getSecond();
+        return hour+":"+minute+":"+sec;
+    }
+
+    protected void waitForVisibilityOfElement(By locator) {
+        wait.until(ExpectedConditions.visibilityOf(find(locator)));
+    }
+
+    public WebElement waitForElementPresence(By locator) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public WebElement fluentWait(final By locator, int timeoutSeconds) {
+        WebElement targetElem = null;
+        try {
+            @SuppressWarnings("deprecation")
+            Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(timeoutSeconds))
+                    .pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+            targetElem = wait.until(new Function<WebDriver, WebElement>() {
+                public WebElement apply(WebDriver driver) {
+                    return find(locator);
+                }
+            });
+            highlightElement(targetElem);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+        return targetElem;
+    }
+
+    public WebDriver switchToWindow(int browserIndex) {
+        try {
+            Set<String> allBrowsers = driver.getWindowHandles();
+            Iterator<String> iterator = allBrowsers.iterator();
+            List<String> windowHandles = new ArrayList<>();
+            while (iterator.hasNext()) {
+                String window = iterator.next();
+                windowHandles.add(window);
+            }
+            // switch to index N
+            driver.switchTo().window(windowHandles.get(browserIndex));
+            highlightElement(By.tagName("body"));
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+        return driver;
+    }
+
+
+    public void uploadFile(By by, String fileRelativePath) {
+        try {
+            WebElement fileUploadElem = driver.findElement(by);
+            highlightElement(fileUploadElem);
+            File tempFile = new File(fileRelativePath);
+            String fullPath = tempFile.getAbsolutePath();
+            logger.info("file uploading : {}", fullPath);
+            fileUploadElem.sendKeys(fullPath);
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public Alert isAlertPresent() {
+        Alert alert = null;
+        try {
+            alert = driver.switchTo().alert();
+            logger.info("Popup Alert present: {{}}", alert.getText());
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+        return alert;
+    }
+
+    public void closeAlert(){
+        try {
+            Alert alert = driver.switchTo().alert();
+            logger.info("Popup Alert closed: {{}}", alert.getText());
+            alert.dismiss();
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
+
+    public void acceptAlert(){
+        try {
+            Alert alert = driver.switchTo().alert();
+            logger.info("Popup Alert accepted: {{}}", alert.getText());
+            alert.accept();
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+        }
+    }
 }
